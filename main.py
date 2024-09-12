6cd21de7f245082adbb2f04d01129315776b7c2a
nemonn <teamnemo78@gmail.com>
2024-09-12 10:13:21 +0300

    Added credit notes and building steps

---
+++ main.py
@@ -0,0 +1,28 @@
+#
+#
+# Bablos Biscuit Hunt - Small practice with Python
+# By Niko Nmaki 2024
+#
+# Inspired By @bablo_thedog - https://www.instagram.com/bablo_thedog/
+#
+#
+# Building steps: (just note to myself)
+# 1. Import Libraries
+# 2. Pygame setup - define canvas size, title etc
+# 3. Define background and set position
+# 4. Define characters 
+# 5. Set coordinates to spawn moving character
+# 6. Draw actors and set draw() -function
+# 7. Define update() -function which acts like gameloop
+# 8. Map keyboard and set key-functions on update() -function
+# 9. Set characters moving speed with velocity -variable
+# 10. Set player character movement animation
+# 11. Define spawn-function for falling object
+# 12. Set schedule for object spawning
+# 13. Set parameters (velocity) for falling objects
+# 14. Add collecting feature with .colliderect() -method
+# 15. Make display to show game info and set timer and life counter and score
+# 16. Set game_over() -details
+# 17. Set start/restart
+# 18. Set music and sounds
+#
